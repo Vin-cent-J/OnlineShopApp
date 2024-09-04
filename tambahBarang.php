3cc3567cfa4f9eb574db0fc5796f5c44151601d5
Vin-cent-J <vincent.joewono123@gmail.com>
2024-09-05 00:12:18 +0700

    API tambah barang

---
+++ tambahBarang.php
@@ -0,0 +1,23 @@
+<?php
+require_once "connect.php";
+
+extract($_POST);
+if(isset($nama, $stok, $harga, $foto, $mereks_id, $kategoris_id)) {
+    $sql = "insert into barangs(nama, stok, harga, foto, mereks_id, kategoris_id) values(?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sidsii", $nama, $stok, $harga, $foto, $mereks_id, $kategoris_id);
+    $stmt->execute();
+
+    $arr = [];
+    if ($stmt->affected_rows){
+        $arr = ["hasil"=>"success", "data"=>"Berhasil menambahkan barang."];
+    }
+    else{
+        $arr = ["hasil"=> "err","data"=> "Gagal menambahkan barang."];
+    }
+    echo json_encode($arr);
+    $stmt->close();
+    $conn->close();
+} else{
+    echo json_encode(["hasil"=>"err", "data"=>"Data belum terisi semua."]);
+}
