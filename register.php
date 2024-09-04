c74c0067518c289e00be2a351557794579e5e652
Vin-cent-J <vincent.joewono123@gmail.com>
2024-09-05 00:12:08 +0700

    Pengecekan variabel

--- register.php
+++ register.php
@@ -2,21 +2,24 @@
 require_once "connect.php";
 
 extract($_POST);
-$perans_id = 1;
-$password = hash("sha256", $password);
-$sql = "insert into penggunas(perans_id, nama, nomor_hp, password, alamat) values(?,?,?,?,?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("issss", $perans_id, $nama, $nomor_hp, $password, $alamat);
-$stmt->execute();
+if(isset($nama, $nomor_hp, $password, $alamat)) {
+    $perans_id = 1;
+    $password = hash("sha256", $password);
+    $sql = "insert into penggunas(perans_id, nama, nomor_hp, password, alamat) values(?,?,?,?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("issss", $perans_id, $nama, $nomor_hp, $password, $alamat);
+    $stmt->execute();
 
-$arr = [];
-if ($stmt->affected_rows){
-    $arr = ["result"=>"success", "data"=>"Berhasil menambahkan pengguna"];
+    $arr = [];
+    if ($stmt->affected_rows){
+        $arr = ["hasil"=>"success", "data"=>"Berhasil menambahkan pengguna."];
+    }
+    else{
+        $arr = ["hasil"=> "err","data"=> "Nomor handphone sudah digunakan."];
+    }
+    echo json_encode($arr);
+    $stmt->close();
+    $conn->close();
+} else{
+    echo json_encode(["hasil"=>"err", "data"=>"Data belum terisi semua."]);
 }
-else{
-    $arr = ["result"=> "err","data"=> "Nomor handphone sudah digunakan."];
-}
-echo json_encode($arr);
-$stmt->close();
-$conn->close();
-?>
