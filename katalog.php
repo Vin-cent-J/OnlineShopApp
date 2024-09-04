751decb8f5c21b22d6044209dbb918359c21b3d5
Vin-cent-J <vincent.joewono123@gmail.com>
2024-09-05 00:11:41 +0700

    API katalog

---
+++ katalog.php
@@ -0,0 +1,43 @@
+<?php
+require_once "connect.php";
+
+extract($_GET);
+
+$sql = "select b.* from barangs b 
+inner join mereks m on b.mereks_id = m.id 
+inner join kategoris k on b.kategoris_id = k.id where b.nama like '%?%' and m.nama like ? and k.nama like ? and b.harga >= ? and b.harga <= ?";
+
+if(!isset($cari)){
+    $cari = "";
+}
+if(!isset($merek)){
+    $merek = "%";
+}
+if(!isset($kategori)){
+    $kategori = "%";
+}
+if(!isset($minHarga)){
+    $minHarga = 0;
+}
+if(!isset($maxHarga)){
+    $maxHarga = 10000000;
+}
+
+
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssdd", $cari, $merek, $kategori, $minHarga, $maxHarga);
+$stmt->execute();
+$hasil = $stmt->get_result();
+
+$arr = [];
+if($row = $hasil->fetch_assoc()){
+    $arr = ["hasil"=>"success", "data"=>$row["nama"]];
+}
+else{
+    $arr = ["hasil"=> "err","data"=> "Barang tidak ditermukan."];
+}
+echo json_encode($arr);
+$stmt->close();
+$conn->close();
+?>
