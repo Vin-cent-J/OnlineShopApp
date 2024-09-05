b1d54cf51e8f46c394b73182f2b44d116394b79a
Vin-cent-J <vincent.joewono123@gmail.com>
2024-09-06 01:00:57 +0700

    API hapus barang

---
+++ API/hapusBarang.php
@@ -0,0 +1,24 @@
+<?php
+require_once "connect.php";
+
+extract($_POST);
+if(isset($id)) {
+    $tgl = date("Y-m-d");
+    $sql = "update barangs set tgl_hapus = ? where id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("si", $tgl, $id);
+    $stmt->execute();
+
+    $arr = [];
+    if ($stmt->affected_rows){
+        $arr = ["hasil"=>"success", "data"=>"Berhasil menghapus barang."];
+    }
+    else{
+        $arr = ["hasil"=> "err","data"=> "Gagal menghapus barang."];
+    }
+    echo json_encode($arr);
+    $stmt->close();
+    $conn->close();
+} else{
+    echo json_encode(["hasil"=>"err", "data"=>"Data belum terisi."]);
+}
