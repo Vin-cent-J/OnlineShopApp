c918f68268bd969a59f918148ff724cdc5ab3e60
Vin-cent-J <vincent.joewono123@gmail.com>
2024-11-03 15:54:38 +0700

    Transaksi template

--- aplikasi/src/app/transaksi/transaksi.page.ts
+++ aplikasi/src/app/transaksi/transaksi.page.ts
@@ -11,5 +11,25 @@ export class TransaksiPage implements OnInit {
 
   ngOnInit() {
   }
+  transaksi = [
+    { id: '123456789', total: 99.99, date: 'November 3, 2024', status: 'Terkirim' },
+    { id: '987654321', total: 49.99, date: 'November 1, 2024', status: 'Menunggu pengiriman' },
+    { id: '456789123', total: 199.99, date: 'October 30, 2024', status: 'Bayar' },
+    { id: '456789122', total: 199.99, date: 'October 30, 2024', status: 'Batal' },
+  ];
 
+  warnaStatus(status: string) {
+    switch (status) {
+      case 'Terkirim':
+        return 'success';
+      case 'Menunggu pengiriman':
+        return 'warning';
+      case 'Bayar':
+        return 'warning';
+      case 'Batal':
+        return 'danger';
+      default:
+        return 'medium';
+    }
+  }
 }
